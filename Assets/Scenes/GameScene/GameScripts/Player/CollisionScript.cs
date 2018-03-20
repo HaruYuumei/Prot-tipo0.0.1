165dcb07f70a5ceb3424e4e5d491a7b70e2ef9e4
HaruYuumei <37444246+HaruYuumei@users.noreply.github.com>
2018-03-20 14:29:59 -0300

    nothing new

---
+++ Assets/Scenes/GameScene/GameScripts/Player/CollisionScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionScript : MonoBehaviour {
+
+    Collision PlayerCol;
+    GameObject Player;
+
+
+	// Use this for initialization
+	void Start () {
+		Player = GameObject.Find("Player");
+
+        PlayerCol = Player.GetComponent<Collision>();
+	}
+
+
+    private void OnCollisionEnter(Collision PlayerCol)
+    {
+        Debug.Log("Colidiu");
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
