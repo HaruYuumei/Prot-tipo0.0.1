40c8b4fe47b31462874ab4069afa2041d339e8f4
HaruYuumei <37444246+HaruYuumei@users.noreply.github.com>
2018-03-24 09:14:40 -0300

    topper

---
+++ Assets/Scenes/GameScene/GameScripts/Follower/Follow.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour {
+
+    GameObject Jogador;
+    Transform DoJogador;
+
+    Rigidbody RbCubo;
+
+    Vector3 nv;
+   
+
+    bool following = false;
+
+
+	// Use this for initialization
+	void Start () {
+        Jogador = GameObject.Find("Player");
+        DoJogador = Jogador.GetComponent<Transform>();
+
+        RbCubo = GetComponent<Rigidbody>();
+	}
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Colidiu");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(Input.GetKey(KeyCode.E))
+        {
+            following = true;
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+
+        if (following == true)
+        {
+            nv.x = DoJogador.position.x - transform.position.x;
+            nv.y = DoJogador.position.y * 0;
+            nv.z = DoJogador.position.z - transform.position.z;
+            nv.Normalize();
+            transform.position += nv * Time.deltaTime;
+        }
+
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
